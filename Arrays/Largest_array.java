1b82f664344c14679435398a5e32a874952b1f23
Arkaprava Dey <arkadey2006@gmail.com>
2026-08-19 22:20:25 +0530

    Updated Largest array and removed Linear Search

--- Arrays/Largest_array.java
+++ Arrays/Largest_array.java
@@ -6,13 +6,20 @@ public class Largest_array {
     public static int getlargest(int numbers[]){
 
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE; 
+
         for(int i=0;i<numbers.length;i++){
 
             if(largest < numbers[i]){
 
                 largest = numbers[i];
             }
+            if(smallest>numbers[i]){
+                smallest = numbers[i];
+            }
         }
+
+        System.out.println("smallest value: "+smallest);
         return largest;
     }
 
