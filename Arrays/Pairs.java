f4874b516d3801d62580e7134e35c8b7df99cecd
Arkaprava Dey <arkadey2006@gmail.com>
2026-09-24 20:13:30 +0530

    Update Pairs array program

--- Arrays/Pairs.java
+++ Arrays/Pairs.java
@@ -2,14 +2,19 @@
 package Arrays;
 
 public class Pairs {
-    public static void printPairs(int numbers[]){
+    
+    public static void printPairs(int numbers[])
+    {
+        int tp =0;
         for(int i=0;i<numbers.length;i++){
             int curr = numbers[i];
             for(int j=i+1;j<numbers.length;j++){
                     System.out.print("(" + curr + "," + numbers[j] + ")");
-            }
+                    tp++;
+                }
             System.out.println();
         }
+        System.out.println("Total Pairs: "+tp);
     }
 
 
